aeecb3ea62d64bc23d89e9b7d439582dcf925d15
Valdir <valdir@substractum.com.br>
2018-07-06 14:52:58 -0300

    ajustes

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import { Input, Row } from 'react-materialize'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="row">
-        <div class="col s12 m4 l3">
-          <div className="input-field col s6">
-            <input placeholder="Placeholder" id="first_name" type="text" className="validate" />
-            <label for="first_name">First Name</label>
+
+      <Row>
+        <div class="col s12">
+
+          <div className="input-field col s2"> 
+            <Input type='text' label="Tag"/>
           </div>
+
+          <div className="input-field col s2">
+            <Input type='number' label="Limit"/>
+          </div>
+
+          <div className="input-field col s2"> 
+            <Input type='number' label="Score"/>
+          </div>
+
+          <div class="input-field col s2">
+
+            <Input type='select' label="Sort">
+              <option value="1">Activity</option>
+              <option value="2">Votes</option>
+              <option value="3">Creation</option>
+              <option value="3">Hot</option>
+              <option value="3">Week</option>
+              <option value="3">Month</option>
+            </Input>
+
+          </div>
+
         </div>
-      </div>
+      </Row>
+
+
     );
   }
 }
